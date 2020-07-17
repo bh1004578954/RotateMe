b04e75810b74567e4fbddcb494a9b8e0bc235224
包涵 <1004578954@qq.com>
2020-07-17 18:26:57 +0800

    update

---
+++ Scripts/Runtime/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float angularVol = 10;
+    void Update()
+    {
+        var rot = Quaternion.AngleAxis(angularVol * Time.deltaTime, Vector3.up);
+        transform.localRotation = rot * transform.localRotation;
+    }
+}
