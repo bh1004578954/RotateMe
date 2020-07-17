ad3461bf4921ab59b2d1a93ca99af6a9ddf73f15
包涵 <1004578954@qq.com>
2020-07-17 18:26:57 +0800

    update

---
+++ Assets/RotateMe/Scripts/Editor/Editor_Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Rotate))]
+public class Editor_Rotate : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+    }
+}
