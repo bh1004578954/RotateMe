4673ae49c122f04f79085b123d9588a30543e57a
包涵 <1004578954@qq.com>
2020-07-21 13:56:56 +0800

    update

--- Assets/RotateMe/Scripts/Runtime/Rotate.cs
+++ Assets/RotateMe/Scripts/Runtime/Rotate.cs
@@ -1,13 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Rotate : MonoBehaviour
 {
+    public UnityEvent Event;
+
+    public GameObject obj;
     public float angularVol = 10;
+    GameObject GORot;
+    private void Start()
+    {
+        GORot =Instantiate(obj, transform);
+    }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Event.Invoke();
+        }
+
+        if (GORot == null) return;
         var rot = Quaternion.AngleAxis(angularVol * Time.deltaTime, Vector3.up);
-        transform.localRotation = rot * transform.localRotation;
+        GORot.transform.localRotation = rot * GORot.transform.localRotation;
     }
 }
