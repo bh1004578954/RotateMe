9d051e4ce03a4b52240f065bbc8a1d1b73f511c4
包涵 <1004578954@qq.com>
2020-07-21 13:56:56 +0800

    update

---
+++ Scripts/Runtime/Test1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class Test1 : MonoBehaviour
+{
+    public void TestEvent()
+    {
+        Debug.Log("event");
+    }
+}
