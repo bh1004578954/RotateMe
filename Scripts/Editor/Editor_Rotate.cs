e85cdacc5b5df68d6decae6743fa77140d52bdfc
包涵 <1004578954@qq.com>
2020-07-23 17:50:26 +0800

    a

--- Scripts/Editor/Editor_Rotate.cs
+++ Scripts/Editor/Editor_Rotate.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,6 +10,11 @@ public class Editor_Rotate : Editor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        
+        Rotate rotate = (Rotate)target ;
+
+        if (GUILayout.Button("Set obj"))
+        {
+            rotate.SetObj(); ;
+        }
     }
 }
