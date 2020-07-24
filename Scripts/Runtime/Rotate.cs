5dcc34d4a70b92566680a0e76c708004f07c525e
包涵 <1004578954@qq.com>
2020-07-24 13:55:20 +0800

    a

--- Scripts/Runtime/Rotate.cs
+++ Scripts/Runtime/Rotate.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 using UnityEditor;
 public class Rotate : MonoBehaviour
 {
-    public UnityEvent Event; 
+    public UnityEvent Event;
 
     public static GameObject obj;
     public float angularVol = 10;
@@ -21,7 +21,7 @@ public class Rotate : MonoBehaviour
         {
             Event.Invoke();
         }
-         
+
         if (GORot == null) return;
         var rot = Quaternion.AngleAxis(angularVol * Time.deltaTime, Vector3.up);
         GORot.transform.localRotation = rot * GORot.transform.localRotation;
@@ -29,9 +29,17 @@ public class Rotate : MonoBehaviour
     [MenuItem("AssetDatabase/LoadAssetExample")]
     public static void SetObj()
     {
-        GameObject go = AssetDatabase.LoadAssetAtPath<GameObject>("Package/com.autocore.rotate-me/Prefabs/Cube.prefab");
-        Debug.Log(Path.GetFileName("Package / com.autocore.rotate - me / Prefabs / Cube.prefab"));
-        if (go == null) Debug.Log("null");
+        GameObject go = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/com.autocore.rotate-me/Prefabs/Cube.prefab");
+        if (go == null)
+        {
+            Debug.Log("null1");
+            go = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/RotateMe/Prefabs/Cube.prefab");
+            //go = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Character.prefab", typeof(GameObject)) as GameObject;
+        }
+        if (go == null)
+        {
+            Debug.Log("null");
+        }
         else Debug.Log(go.name);
         Instantiate(go);
         obj = go;
